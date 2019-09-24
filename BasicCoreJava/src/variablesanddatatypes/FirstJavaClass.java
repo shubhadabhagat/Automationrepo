c5954d4f321a7d9e45f4f02e64f48119d77bf2c3
shubhada <shubhadanagrale@gmail.com>
2019-09-25 00:09:49 +0530

    variablesanddatatypesnew

--- BasicCoreJava/src/variablesanddatatypes/FirstJavaClass.java
+++ BasicCoreJava/src/variablesanddatatypes/FirstJavaClass.java
@@ -1,24 +1,26 @@
 package variablesanddatatypes;
 
-public class FirstJavaClass {
+public class firstjavaclass {
 
 	public static void main(String[] args) {
 		
 		//widely used datatypes
-		int a=10; //For integer values
-		char b='A'; //For single characters
-		boolean c=true;//For boolean values
-		double d=6.89;//For decimal values
-		String name="Shubhada";//For string values**string is not data type
-		
-		//Other datatypes
-		//For numbers
-		byte x=10;
-		short y=45;
-		long z=6796896;
-		
-		//For decimal values
-		float f=67.8f;
-		System.out.println(a);
+				int a=10; //For integer values
+				char b='A'; //For single characters
+				boolean c=true;//For boolean values
+				double d=6.89;//For decimal values
+				String name="Shubhada";//For string values**string is not data type
+				
+				//Other datatypes
+				//For numbers
+				byte x=10;
+				short y=45;
+				long z=6796896;
+				
+				//For decimal values
+				float f=67.8f;
+				System.out.println(a);
+
 	}
+
 }
