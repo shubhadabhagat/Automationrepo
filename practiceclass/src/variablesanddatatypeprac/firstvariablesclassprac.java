94b2d9c180ae624e8dfd9235c8ce12662bcf619f
shubhada <shubhadanagrale@gmail.com>
2019-09-25 00:31:40 +0530

    firstvariableclassprac

---
+++ practiceclass/src/variablesanddatatypeprac/firstvariablesclassprac.java
@@ -0,0 +1,23 @@
+package variablesanddatatypeprac;
+
+public class firstvariablesclassprac {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		char b='A';
+		boolean c=true;
+		double d=6.89;
+		String name="Shubhada";
+		
+		//for numbers
+		byte x=10;
+		short y=45;
+		long z=6796896;
+		
+		float f=67.8f;
+	System.out.println(z);
+
+	}
+
+}
