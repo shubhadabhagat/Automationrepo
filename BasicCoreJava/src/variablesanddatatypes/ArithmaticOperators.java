4e95dfb8d3f2f6cd9e8d9209cd04818c767b93df
shubhada <shubhadanagrale@gmail.com>
2019-09-25 21:57:20 +0530

    Arithmatic operators

---
+++ BasicCoreJava/src/variablesanddatatypes/ArithmaticOperators.java
@@ -0,0 +1,26 @@
+package variablesanddatatypes;
+
+public class ArithmaticOperators {
+
+	public static void main(String[] args)
+	{
+		
+		int a=10;
+		int b=20;
+		
+		int sum=a+b;
+		System.out.println(sum);
+
+		int sub=b-a;
+		System.out.println(sub);
+		
+		int div=b/a;
+		System.out.println(div);
+		
+		int mul=a*b;
+		System.out.println(mul);
+		int mod=a%b;
+		System.out.println(mod);
+	}
+
+}
