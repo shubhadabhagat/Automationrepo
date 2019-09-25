61e5da25aca23dcd812a5d3af452bcc870cdb562
shubhada <shubhadanagrale@gmail.com>
2019-09-25 21:58:03 +0530

    conditionaloperatorAnd

---
+++ BasicCoreJava/src/conditionalstatements/ConditionalOperatorAnd.java
@@ -0,0 +1,17 @@
+package conditionalstatements;
+
+public class ConditionalOperatorAnd {
+
+	public static void main(String[] args) {
+		double pcm=60.0;
+		double cet=30.0;
+		if(pcm>=60.0 && cet>=50.0)
+		{
+			System.out.println("You are eligible");
+		}else {
+			System.out.println("You are not eligible");
+		}
+
+	}
+
+}
