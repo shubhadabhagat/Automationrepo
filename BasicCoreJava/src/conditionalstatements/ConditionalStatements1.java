084383048600c15ee8359983f0441fb47699d8b9
shubhada <shubhadanagrale@gmail.com>
2019-09-25 21:55:15 +0530

    conditionalStatement1

---
+++ BasicCoreJava/src/conditionalstatements/ConditionalStatements1.java
@@ -0,0 +1,20 @@
+package conditionalstatements;
+
+public class ConditionalStatements1 {
+
+	public static void main(String[] args)
+	{
+	    int a=10;
+	    int b=20;
+	   
+	    System.out.println(a>b);
+	    System.out.println(b>a);
+	    if(a>b)
+	    {
+	    	System.out.println("a is greater than b");
+	    }else {
+	    	System.out.println("b is greater than a");
+	    }
+	}
+
+}
