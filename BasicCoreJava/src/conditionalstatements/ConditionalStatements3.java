0abe4a4a3e548da3ba94b4e892bece2c77f02de1
shubhada <shubhadanagrale@gmail.com>
2019-09-25 21:56:44 +0530

    Conditionalstatement3

---
+++ BasicCoreJava/src/conditionalstatements/ConditionalStatements3.java
@@ -0,0 +1,16 @@
+package conditionalstatements;
+
+public class ConditionalStatements3 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		if (a == b) {
+			System.out.println("a is equal to b");
+		} else {
+			System.out.println("a is not equal to b");
+		}
+
+	}
+
+}
