fefcd33c51a94dbc59f83fc6906ec7539a797b17
shubhada <shubhadanagrale@gmail.com>
2019-09-27 01:48:40 +0530

    to print star in matrix form

---
+++ BasicCoreJava/src/loops/ques2printstar.java
@@ -0,0 +1,18 @@
+package loops;
+
+public class ques2printstar {
+
+	public static void main(String[] args) {
+		
+		String name="*";
+	for(int i=1;i<=4;i++)
+	{
+		for(int j=1;j<=4;j++) {
+			System.out.print(name+" ");
+		}System.out.println();
+	}
+	
+
+	}
+
+}
