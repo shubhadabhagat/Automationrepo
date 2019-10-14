53b80b366b6225b15ce11cd31c9d60c9da817dbf
shubhada <shubhadanagrale@gmail.com>
2019-10-14 21:20:14 +0530

    Selenium-How to use Locators for Elements

--- NewSeleniumProject/src/basicSelenium/SeleniumClass1.java
+++ NewSeleniumProject/src/basicSelenium/SeleniumClass1.java
@@ -1,15 +1,39 @@
 package basicSelenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class SeleniumClass1 {
 	@Test
-	public void login() {
+	public void login() throws InterruptedException {
 	System.out.println("Lauch Chrome Browser");	
 	System.setProperty("webdriver.chrome.driver", "C:\\techbodhi\\Drivers\\chromedriver.exe");
 	ChromeDriver dr=new ChromeDriver();
-	dr.get("http://google.com");
+	dr.get("http://www.newtours.demoaut.com");
+	
+	//To maximize the window of output 
+	dr.manage().window().maximize();
+	WebElement username=dr.findElement(By.name("userName"));
+	
+	//To Identify Element(Textbox)
+	username.sendKeys("mercury");
+	username.clear();
+	username.sendKeys("mercury");
+	
+	//Thread.sleep(2000);
+	//username.clear();
+	//Thread.sleep(2000);
+	
+	//Textbox
+	dr.findElement(By.name("password")).sendKeys("mercury");
+	//Button
+	dr.findElement(By.name("login")).click();
+	//Radio button
+	System.out.println(dr.findElement(By.xpath("//input[@value='oneway']")).isSelected());
+	dr.findElement(By.xpath("//input[@value='oneway']")).click();
+	System.out.println(dr.findElement(By.xpath("//input[@value='oneway']")).isSelected());
 	
 	}
 
